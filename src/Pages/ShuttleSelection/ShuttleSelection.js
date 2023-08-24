b7996669ba51e79ec6d8cf5d8ad41fe35141a992
Sahana Narayan <sahana.narayan@sahana-narayan-ft7d66dxdr.lan>
2023-08-23 23:19:07 -0700

    added counter

--- src/Pages/ShuttleSelection/ShuttleSelection.js
+++ src/Pages/ShuttleSelection/ShuttleSelection.js
@@ -13,19 +13,24 @@ import { useNavigate } from "react-router-dom";
 import MockScreenshot from '../../Components/MockScreenshot/MockScreenshot.js';
 import Image from './shuttleselection.png'; // another image is defined here using file path
 import './ShuttleSelection.css';
+import Counter from './counter.js';
+
 
 // this is some random bullshit combination of Base UI components and logic to demonstrate what a UI might look like in React
 // since they don't care about backend you can probably just store persisting data in React state
 
+
+
+  
 function ShuttleSelection() {
     const [stepState, setStepState] = useState(0);
     const [userAnswer, setUserAnswer] = useState('');
     let navigate = useNavigate();
     const buttonStyle = {
-        marginTop: '660px', // Adjust the margin-top value as needed
+        marginTop: '695px', // Adjust the margin-top value as needed
         marginLeft: '-150px',
         padding: '30px 250px',
-        backgroundColor: 'transparent'
+        //backgroundColor: 'transparent'
     }
     return (
         <div className='ShuttleSelection page'>
@@ -33,6 +38,7 @@ function ShuttleSelection() {
             <Button size={SIZE.large} onClick={() => { navigate(`/confirmation`) }} style={buttonStyle}>
                     {/*make sure navigate param matches path defined in App.js*/}
             </Button>
+            <Counter />
             </MockScreenshot>
         </div>
     );
